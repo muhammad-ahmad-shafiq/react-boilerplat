f9892bb46342bbe49d46cdb2888e16d6e00c7d1b
Ahmad Shafiq <mahmadshafiq786@gmail.com>
2020-02-06 06:31:01 -0800

    Babel: Added babel transcompiler which translates our JS(even in ES6) to standard JS

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,16 @@ module.exports = {
     path: path.join(__dirname, 'public'),
     filename: 'bundle.js'
   },
+  module: {
+    rules: [
+      // Load JS with Babel
+      {
+        test: /\.js$/,
+        include: path.resolve(__dirname, 'src'),
+        loader: 'babel-loader'
+      }
+    ]
+  },
   devtool: 'cheap-module-eval-source-map',
   devServer: {
     port: 4000,
